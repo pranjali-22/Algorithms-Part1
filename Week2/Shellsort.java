8cac5e93078478abec2f895c8feacb8e12511e62
pdas05 <pdas05@students.cs.ubc.ca>
2024-06-08 06:42:47 -0700

    Shellsort

--- Week2/Shellsort.java
+++ Week2/Shellsort.java
@@ -1,16 +1,28 @@
 public class Shellsort {
     public static void sort(Comparable[] a) {
-        int N = a.length;
         int h = 1;
-        while (h < N/3) h = 3*h + 1; 
-        while (h >= 1){ 
-            for (int i = h; i < N; i++)
-            {
-                for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
-                exchange(a, j, j-h);
+        int len = a.length;
+        while (h < len/3){
+            h = 3*h + 1;
+        } 
+        
+        // i - current pointer
+        // next pointer = i + h
+        // prev pointer = i - h
+        while (h > 0) {
+            System.out.println("h: "+ h);
+            for(int i = 0; (i+h < len); i++) {
+                if(less(a[i+h],a[i])){
+                    exchange(a,i,i+h);
+                }
+                for(int j = i; j-h >= 0; j = j-h){
+                    if(less(a[j],a[j-h])){
+                        exchange(a,j-h,j);
+                    }
+                }
             }
-            h = h/3;
- }
+            h = (h-1)/3;
+        }
 
     }
     public static boolean less(Comparable v, Comparable w) {
